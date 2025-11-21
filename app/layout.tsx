d02bcaf23142d4cff1c9866502b04e9829175fe8
Christian Velez <christian.gerardo_@outlook.es>
2025-11-21 16:22:59 -0600

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,35 +1,55 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import { siteConfig } from "@/config";
+import type { Metadata } from 'next'
+import { Geist, Geist_Mono } from 'next/font/google'
+import './globals.css'
+import { siteConfig } from '@/config'
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+})
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+})
+
+const DOMAIN_URL = process.env.NEXT_PUBLIC_BASE_URL!
 
 export const metadata: Metadata = {
-  title: siteConfig.name,
+  applicationName: siteConfig.name,
   description: siteConfig.description,
-};
+  metadataBase: new URL(DOMAIN_URL),
+  openGraph: {
+    title: {
+      default: siteConfig.name,
+      template: `%s - ${siteConfig.name}`,
+    },
+    description: siteConfig.description,
+    locale: 'es_MX',
+    url: DOMAIN_URL,
+    siteName: siteConfig.name,
+    type: 'website',
+  },
+
+  twitter: {
+    card: 'summary',
+    title: siteConfig.name,
+    description: siteConfig.description,
+  },
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
     </html>
-  );
+  )
 }
